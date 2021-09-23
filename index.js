3d82cf721e7ba322533029a8603be63fecfaa311
CommentatoreVideo <54153405+CommentatoreVideo@users.noreply.github.com>
2021-09-23 17:31:22 +0200

    Vercel

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 3000, () => console.log("Server partito"));
+module.exports = app;
